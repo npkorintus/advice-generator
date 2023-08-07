61fdb40928cc0cbada23ac77fdb5085def5a6226
Nick Korintus <nicholas.korintus@gmail.com>
2023-08-06 22:04:33 -0400

    added challenge files

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -3,7 +3,13 @@ import AdviceCard from '../AdviceCard';
 
 const Layout = () => {
   return (
-    <AdviceCard />
+    <>
+      <AdviceCard />
+      <div class="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+        Coded by <a href="#">Your Name Here</a>.
+      </div>
+    </>
   );
 };
 
