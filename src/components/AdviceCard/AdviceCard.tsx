c36befa2901c0943c37ec830001c0d01cf6aacea
Nick Korintus <nicholas.korintus@gmail.com>
2023-08-08 17:56:45 -0400

    cleaned up stylesheets, removed inline styles

--- src/components/AdviceCard/AdviceCard.tsx
+++ src/components/AdviceCard/AdviceCard.tsx
@@ -29,51 +29,15 @@ const AdviceCard: React.FC = () => {
 
   return (
     loading ? <CircularProgress /> : (
-      <div
-        className='advice-card'
-        style={{
-          // border: '1px solid black',
-          padding: '20px',
-          borderRadius: '12px',
-          background: 'hsl(217, 19%, 24%)'
-          // background: 'hsl(217, 19%, 38%)'
-        }}
-      >
-        <div
-          style={{
-            color: 'hsl(150, 100%, 66%)',
-            textTransform: 'uppercase',
-            letterSpacing: '4px'
-          }}
-        >Advice #{adviceSlip.slip.id}</div>
-        <div
-          style={{
-            color: 'hsl(193, 38%, 86%)',
-            fontWeight: 800,
-            fontSize: '28px',
-            marginTop: '32px'
-          }}><q>{adviceSlip.slip.advice}</q></div>
-        <div>
-          <img
-          src='../../../images/pattern-divider-mobile.svg'
-          style={{
-            marginTop: '20px',
-            marginBottom: '20px'
-          }}
-          />
+      <div className='advice-card'>
+        <div className='id'>Advice #{adviceSlip.slip.id}</div>
+        <div className='advice'><q>{adviceSlip.slip.advice}</q></div>
+        <div className='divider'>
+          <img src='../../../images/pattern-divider-mobile.svg' />
         </div>
-        <div style={{ height: '10px' }}>
-          <span className='dot'
-            style={{
-              height: '60px',
-              width: '60px',
-              backgroundColor: 'hsl(150, 100%, 66%)',
-              borderRadius: '50%',
-              display: 'inline-block',
-              transform: 'translateY(50px)'
-            }}
-          >
-            <img src="../../../images/icon-dice.svg" style={{ transform: 'translateY(16px)' }} />
+        <div className='dot-container'>
+          <span className='dot'>
+            <img className='dice-img' src="../../../images/icon-dice.svg" />
           </span>
         </div>
       </div>
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -5,13 +5,7 @@ const Layout: React.FC = () => {
   return (
     <div className='layout'>
       <AdviceCard />
-      <div className="attribution"
-        style={{
-          position: 'absolute',
-          bottom: '16px',
-          color: 'white'
-        }}
-      >
+      <div className="attribution">
         Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
         Coded by <a href="#">Nick Korintus</a>.
       </div>
